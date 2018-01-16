fe8887f8957e7c4cec32a4eec645c26bc1daaf5c
sbudu <s.budumyan@gmail.com>
2018-01-16 16:49:07 +0400

    Autoload w/ config

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+define("IMG_PATH", __DIR__ . DIRECTORY_SEPARATOR .'data' . DIRECTORY_SEPARATOR );
+define("DB_PATH", __DIR__ . DIRECTORY_SEPARATOR .'data' . DIRECTORY_SEPARATOR . 'db.sqlite3' );
